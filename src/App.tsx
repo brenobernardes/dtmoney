3adad0527415e59e7816649e4b8911b60c816926
Breno Bernardes <breno.pbernardes@gmail.com>
2023-02-03 20:26:24 -0300

    modal creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactioModalOpen, setisNewTransactioModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setisNewTransactioModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setisNewTransactioModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactioModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+        />
       <GlobalStyle />
     </>
   );
